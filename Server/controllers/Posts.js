c844138218ccd84b1fee01567c4b921727d0794f
ahmed ali <sulubsson@gmail.com>
2022-02-05 15:40:31 +0100

    added routes, controllers to write the logics and modules(PostSchema)

---
+++ Server/controllers/Posts.js
@@ -0,0 +1,15 @@
+import PostMessage from '../modules/postMessage.js'
+
+export const getPosts = async (req,res) => {
+    try{
+        const postMessages = await PostMessage.find();
+        consosle.log(postMessages);
+        res.status(200).json(postMessages);
+    }catch (error){
+        res.status(404).json({message: error.message})
+    }
+}
+
+export const createPost = (req,res)=> {
+    res.send('post Creation')
+}
--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,25 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import mongoose from 'mongoose';
+
+import postRoutes from './routes/Posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit: "30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended:true}))
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://sulubsson:sulubsson123@sulubsson.jql9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+//to prevent future errors
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true, useUnifiedTopology:true})
+//
+.then(()=> app.listen(PORT,() => console.log(`SERVER IS RUNNING ON PORT: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+//to prevent future errors
+// mongoose.set('useFindAndModify', false);
